03ac09470bdca5411807059b5de42955f5a175f7
DeniValuev <valuev.denis91@mail.ru>
2024-08-28 01:50:26 +0300

    Добавьте файлы проекта.

---
+++ Module8.Unit0/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module8.Unit0
+{
+    public class Drive
+    {
+        public string Name { get;}
+        public long TotalSpace { get;}
+        public long FreeSpace { get;}
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+    }
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        }
+    }
+}
